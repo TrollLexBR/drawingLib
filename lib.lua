3b61efaf536cf8d74f244d92c364823ea13b37ee
TrollLexBR <72568672+TrollLexBR@users.noreply.github.com>
2022-07-01 10:52:33 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -972,6 +972,7 @@ function utility.dragify(object)--, dragoutline)
             start = input.Position
             --dragoutline.Visible = false
             objectposition = object.Position
+            object.Position = currentpos
         end
     end)
 
@@ -979,6 +980,7 @@ function utility.dragify(object)--, dragoutline)
         if input.UserInputType == Enum.UserInputType.MouseMovement and dragging then
             currentpos = UDim2.new(objectposition.X.Scale, objectposition.X.Offset + (input.Position - start).X, objectposition.Y.Scale, objectposition.Y.Offset + (input.Position - start).Y)
             input.Position = currentpos
+            object.Position = currentpos
         end
     end)
 
